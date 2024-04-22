9390c9d31896f05406492f197a89067d073f1f19
pammyzm <167824419+pammyzm@users.noreply.github.com>
2024-04-22 19:50:30 -0300

    Add files via upload

---
+++ praticando.js
@@ -0,0 +1,18 @@
+alert ("Bem vindo ao nosso site!");
+let Nome = "Lua";
+let Idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert ("Erro! Preencha todos os campos!")
+let mensagemDeErro = "Erro! Preencha todos os campos"
+alert(mensagemDeErro);
+
+let Nome prompt ("Digite seu nome.");
+let Idade prompt ("Digite sua idade");
+let maiorIdade = prompt("Digite sua idade")
+if (idade >= 18) {
+alert ("Pode tirar habilitação!")
+
+}
+
